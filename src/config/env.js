341d7caedda7bddec14cc10cb6d6c97946fede06
sachin kumar <sachinkumar@sachins-MacBook-Air.local>
2025-11-27 15:18:50 +0530

    commit for mongodb

--- src/config/env.js
+++ src/config/env.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 4000,
-  mongoUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/gst-invoice-db',
+  mongoUri: process.env.MONGODB_URI || 'mongodb+srv://sachin:Sachin123@gst.o916bnn.mongodb.net/?appName=gst',
   jwtSecret: process.env.JWT_SECRET || 'change-this-secret',
   jwtExpire: process.env.JWT_EXPIRE || '7d',
   frontendUrl: process.env.FRONTEND_URL || 'http://localhost:3000',
